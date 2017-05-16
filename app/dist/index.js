239194d70312d6b498f514978db1e045e73eb8e3
Fernando Parada <fernandoparada18@openmailbox.org>
2017-05-16 15:45:35 -0400

    Creacion y uso de componentes Video[5]

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,21 @@
-require('./modulo1.js');
-require('./modulo2.js');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  constructor(){
+    super();
+    this.nombre = "Fernando Parada";
+  }
+  obtenerNombre(){
+    return "Codigo Facilito";
+  }
+  render(){
+    return(
+      <h1>Hola { this.obtenerNombre() }</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
